037edbb289f0b3c4d25e8bb494f20a46764d7f5c
Yosep Puji Rohman <yosepfr@gmail.com>
2022-05-30 16:27:53 +0700

    update

---
+++ python/main.py
@@ -0,0 +1,9 @@
+import time
+start_time = time.time()
+
+print("Hello World!!!!")
+print("Nama saya Yosep Puji Rohman")
+
+print(time.time() - start_time, )
+"""ini adalah
+comment multiline"""
---
+++ python/variable.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+c = 30
+
+print("ini adalah nilai variable a:",a)
+print("ini adalah nilai variable b:",b)
+print("ini adalah nilai variable c:",c)
+
+#variable di atas akan di replace jika ada variable yang sama di bawah
+
+a = "Yosep"
+b = "Puji"
+c = "Rohman"
+
+print("Hello world, nama saya ",a,b,c)
