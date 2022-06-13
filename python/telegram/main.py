116785febe6f1d89971581bbd5ba8f8dca92f69c
Yosep Puji Rohman <yosepfr@gmail.com>
2022-06-13 11:35:03 +0700

    telegram bot

--- python/telegram/main.py
+++ python/telegram/main.py
@@ -1,3 +1,6 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+
 import telegram.ext
 
 TOKEN = "5591863640:AAHqDldhR_IeWV8vvegVa3zsnJ_947XBt0s"
@@ -5,18 +8,17 @@ TOKEN = "5591863640:AAHqDldhR_IeWV8vvegVa3zsnJ_947XBt0s"
 #    TOKEN = f.read()
 
 def start(update, context):
-    update.message.reply_text("Hello!!! Selamat datang di DAO Official!")
-
-def help(update, context):
     update.message.reply_text("""
-    Berikut perintah yang kamu bisa gunakan:
+    Hello!!! Selamat datang di DAO Official!
+    
+    Silahkan pilih menu yang kamu butuhkan:
 
     /start -> Welcome message
     /Restart_QRIS_service -> Restart service
     /Capture_grafana_NDS -> Capture dashboard live AADC NDS
-    /help -> this message
     /contact -> DAO on duty
     """)
+
 
 def Restart_QRIS_service(update, context):
     update.message.reply_text("Maaf, Fitur ini masih dalam tahap pengembangan T_T")
@@ -27,12 +29,10 @@ def Capture_grafana_NDS(update, context):
 def contact(update, context):
     update.message.reply_text("Kontak tim kami di 083829320227")
 
-
 updater = telegram.ext.Updater(TOKEN, use_context=True)
 disp = updater.dispatcher
 
 disp.add_handler(telegram.ext.CommandHandler("start", start))
-disp.add_handler(telegram.ext.CommandHandler("help", help))
 disp.add_handler(telegram.ext.CommandHandler("contact", contact))
 disp.add_handler(telegram.ext.CommandHandler("Restart_QRIS_service", Restart_QRIS_service))
 disp.add_handler(telegram.ext.CommandHandler("Capture_grafana_NDS", Capture_grafana_NDS))
